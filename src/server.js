f169c885f0213317a019acc56c4df6d1fd017a19
DK3XDK2 <hacktyper12@gmail.com>
2025-08-03 12:23:48 +0700

    new

--- src/server.js
+++ src/server.js
@@ -70,7 +70,7 @@ app.post("/login", async (req, res) => {
     req.session.mssv = mssvFromWeb;
     req.session.password = matkhau;
     req.session.isPrincess =
-      mssvFromWeb.trim().toLowerCase() === "dtc245180076";
+      mssvFromWeb.trim().toLowerCase() === "dtc245310153";
 
     fs.writeFileSync(
       `./data/${mssvFromWeb}_lichthi.json`,
